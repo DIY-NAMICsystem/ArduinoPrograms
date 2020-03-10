644977b51e49dd472e2ae3cfa9b279edd7c250c3
Jun Ho Lee <43481979+flagship20030@users.noreply.github.com>
2020-03-10 17:01:06 -0400

    Added Programs for Group 5
    Actual Programs that were run for Group 5 (uploaded for recordkeeping purpose)

---
+++ GROUP_5/G5_P1/m_port_operate.h
@@ -0,0 +1,134 @@
+/*--------------------------------------------------------++
+||                    Operate Left Port                   ||
+++--------------------------------------------------------*/
+
+// Solenoid Reward will only be from the middle port!
+
+void activate_middle_port() {
+
+     static unsigned long led_on_time = 0;
+     static unsigned long led_off_time = 0;
+
+     static unsigned long solenoid_on_time = 0;
+     static unsigned long solenoid_off_time = 0;
+
+     static unsigned long iti_start_time = 0;
+     // static unsigned long reward_retrieval_time = 0;
+
+     // static unsigned long valid_poke_timer = 0;
+     // static unsigned long invalid_poke_timer = 0;
+
+      // solenoid_active_L becomes false only after ITI time expires
+      // Valid trial has started: Turn on left LED
+
+     if (reward_window) {
+
+       // 1. REWARD CUE ON / SOLENOID ON
+         if ((reward_cue_window == false) && (led_state_M == LOW) && (start_iti_window == false)){
+
+           // Reward Cue ON
+           led_state_M = HIGH;
+           digitalWrite(port_led_M, led_state_M);
+
+           // Activate Solenoid
+           sol_state_M = HIGH;
+           digitalWrite(port_solenoid_M, sol_state_M);
+
+           led_on_time = millis();
+           solenoid_on_time = millis();
+           // Serial.print(F("Valid led On:"));
+
+           // Reward Cue ON
+           Serial.print(F("8171:")); // Timestamp for Valid Light On
+           Serial.println(led_on_time);
+
+           Serial.print(F("8271:"));     // MIDDLE SOLENOID ON Timestamp for Valid Solenoid On
+           Serial.println(solenoid_on_time);
+
+           reward_cue_window = true;
+           // solenoid_duration_window = true;
+
+           }
+
+        // 2. SOLENOID OFF
+        if ((sol_state_M == HIGH) && (millis() - solenoid_on_time > solenoid_on_duration)) {
+
+          sol_state_M = LOW;
+          digitalWrite(port_solenoid_M, sol_state_M);
+
+          solenoid_off_time = millis();
+          // Serial.print(F("valid_solenoid_off:"));
+
+          Serial.print(F("8270:"));   // MIDDLE SOLENOID Timestamp for Valid Solenoid Off
+          Serial.println(solenoid_off_time);
+        }
+
+        // 3. REWARD CUE OFF
+        if ((reward_cue_window) && (millis() - led_on_time > reward_cue_duration)) {
+
+          reward_cue_window = false;
+
+          led_state_M = LOW;
+          digitalWrite(port_led_M, led_state_M);
+
+          led_off_time = millis();
+          // Serial.print(F("Invalid led Off:"));
+
+
+          // LED_M_off
+          Serial.print(F("8170:"));    // Omission Trial END Timestamp (LEFT PORT OMISSION)
+          Serial.println(led_off_time);
+
+          start_iti_window = true;
+          iti_start_time = millis();
+
+          // RESET counter for poke counts during iti window
+          left_port_counter = 0;  // reset any port counters before TRIAL WINDOW starts (so that any invalid pokes get resetted during iti window)
+          mid_port_counter = 0;   //  (could also be a measure of incorrect pokes during a certain TIMEFRAME)
+          right_port_counter = 0;
+
+        }
+
+      // START ITI
+       if (start_iti_window) {
+
+          // Initiate Next trial after ITI expires
+          if ((millis() - iti_start_time) >= iti_interval[random_idx]) {
+
+            // # of poke counts during iti window
+            Serial.print("7519::");   // 75xx
+            Serial.println(left_port_counter);
+            Serial.print("8519::");    // 85xx
+            Serial.println(mid_port_counter);
+            Serial.print("9519::");  // 95xx
+            Serial.println(right_port_counter);
+
+            get_random_iti = true;
+            random_idx = random_iti();
+
+            // prints out the next iti (the next iti duration to be used after it loops around)
+            // Serial.print("idx: ");
+            // Serial.println(random_idx);
+            // Serial.println(iti_interval[random_idx]);
+
+
+            // RESETTING counters before new trial starts (to count # of pokes made during trial window)
+            left_port_counter = 0;  // reset any port counters before TRIAL WINDOW starts (so that any invalid pokes get resetted during iti window)
+            mid_port_counter = 0;   //  (could also be a measure of incorrect pokes during a certain TIMEFRAME)
+            right_port_counter = 0;
+
+            // RESET current_port so that leds and solenoids don't get activated again
+            current_port = 0;  // RESET NEEDED!!
+
+            start_iti_window = false;
+            initiate_new_trial = true;
+
+            reward_window = false;
+
+
+          }
+        }
+
+
+     }
+}
---
+++ GROUP_5/G5_P2/begin_trial.h
@@ -0,0 +1,52 @@
+/*--------------------------------------------------------++
+||                     Begin New Trial                    ||
+++--------------------------------------------------------*/
+
+void begin_new_trial() {
+
+  static unsigned long trial_window_start_time = 0;
+  static unsigned long trial_window_end_time = 0;
+
+  static unsigned long iti_window_start_time = 0;
+  static unsigned long iti_window_end_time = 0;
+
+  static unsigned long previous_m_LED_millis=0;
+
+  // Initiate New Trial is toggled on AFTER iti duration expires (from l/R port operate.h)
+
+  // Initiate New Trial (Parent IF)
+  if (initiate_new_trial){
+
+      // transition_to_reward = false;  // so that solenoid doesnt'get activated
+
+    // Trial Window hasn't started yet - If all lights are turned OFF, TURN ON Middle light
+    if ((trial_window == false) && (reward_window == false)){
+
+       // start Trial Window Period
+       trial_window = true;
+
+       // * * DON'T USE for loops for toggling components - always use STATES!! * *
+       // for (byte i = 8; i <=10; i++) { digitalWrite(i, HIGH);}
+
+       // Turn ON Left / Right LED (Stimulus Ports)
+       led_state_L = HIGH;  digitalWrite(port_led_L, led_state_L);
+       led_state_R = HIGH;  digitalWrite(port_led_R, led_state_R);
+
+       trial_window_start_time = millis();
+       Serial.print(F("5521:"));
+       Serial.println(trial_window_start_time);
+
+       }
+
+
+    // Poke in Middle Port(IR.h) initiates Valid Trial:
+
+    // If mouse pokes in MIDDLE port and initiates trial, --> LED turns off!! (all three should turn off!)
+    // Also when delay window didn't start
+    // (MOUSE CAN POKE REGARDLESS OF BLINK CYCLE - either ON or OFF cycle!) --> goverened by the STATE!
+
+    // (Valid trial window toggles TRUE if poke in middle port (see IR.h))
+    // POKE in middle -->  Valid Trial Window
+
+  }
+}
